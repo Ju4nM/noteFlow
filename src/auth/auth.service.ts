048a2d9c8ad526765701fa8a0307ccc6ca47651c
Ju4nM <jenriquemflores@gmail.com>
2024-06-23 20:43:17 -0700

    @types/bcrypt agregado y validadores agregados a loginDto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { LoginDto } from './dto/login.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
-import { compare } from "bcrypt";
+import * as bcrypt from "bcrypt";
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
@@ -24,7 +24,7 @@ export class AuthService {
 
     if (!target) throw new HttpException(null, HttpStatus.UNAUTHORIZED);
 
-    const passwordMatching: boolean = await compare(password, target.password);
+    const passwordMatching: boolean = await bcrypt.compare(password, target.password);
     if (!passwordMatching)  throw new HttpException(null, HttpStatus.UNAUTHORIZED);
 
     const accessToken = await this.jwtService.signAsync({ id: target.id }, { secret: process.env.JWT_SECRET });
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, MaxLength, MinLength } from "class-validator";
 
 export class LoginDto {
+
+  @IsEmail({}, { message: "El email no tiene el formato correcto" })
   email: string;
+
+  @MinLength(8, { message: "La contraseña debe tener al menos 8 caracteres" })
+  @MaxLength(255, { message: "La contraseña puede tener un maximo de 255 caracteres" })
   password: string;
 }
